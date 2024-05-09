c9aa26c881a6beb8895ede7470546bb3cdb7ebdb
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-05-09 21:59:33 +0530

    update 1

--- src/main/java/com/devstack/customerserviceapi/service/CustomerService.java
+++ src/main/java/com/devstack/customerserviceapi/service/CustomerService.java
@@ -1,7 +1,13 @@
 package com.devstack.customerserviceapi.service;
 
 import com.devstack.customerserviceapi.dto.CustomerDto;
+import com.devstack.customerserviceapi.repo.CustomerRepo;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 public interface CustomerService {
+
     public void createCustomer(CustomerDto customerDto);
 }
--- src/main/java/com/devstack/customerserviceapi/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/devstack/customerserviceapi/service/impl/CustomerServiceImpl.java
@@ -1,11 +1,29 @@
 package com.devstack.customerserviceapi.service.impl;
 
 import com.devstack.customerserviceapi.dto.CustomerDto;
+import com.devstack.customerserviceapi.entity.Customer;
+import com.devstack.customerserviceapi.repo.CustomerRepo;
 import com.devstack.customerserviceapi.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepo customerRepo;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
+
     @Override
     public void createCustomer(CustomerDto customerDto) {
-
+        Customer customer = new Customer(
+                customerDto.getId(),customerDto.getName(),customerDto.getAddress(),customerDto.getSalary()
+        );
+        customerRepo.save(customer);
     }
+
 }
